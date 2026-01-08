e3338794aa341e2f8a2fa9af9dfee8b655514ca7
diiego-s <serradiego2005@icloud.com>
2026-01-08 17:34:48 +0100

    Aggiunta schermata iniziale, e collegamento a nuova pagina

--- SuperSpan/app/src/main/java/com/example/superspan/LoginActivity.kt
+++ SuperSpan/app/src/main/java/com/example/superspan/LoginActivity.kt
@@ -1,35 +1,20 @@
 package com.example.superspan
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val btnStart = findViewById<Button>(R.id.btnStart)
-
-        btnStart.setOnClickListener {
-            login()
-        }
-
     }
-
-    fun login(){
-        val intent = Intent(this, LoginActivity::class.java);
-        startActivity(intent);
-    }
-
 }
