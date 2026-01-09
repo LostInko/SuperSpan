ab8e94fd4933bd224114ec17de4d365453e253d6
diiego-s <serradiego2005@icloud.com>
2026-01-09 11:33:27 +0100

    Added register screen and back navigation.

--- SuperSpan/app/src/main/java/com/example/superspan/RegisterActivity.kt
+++ SuperSpan/app/src/main/java/com/example/superspan/RegisterActivity.kt
@@ -1,6 +1,8 @@
 package com.example.superspan
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,17 @@ class RegisterActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val ivBack = findViewById<ImageView>(R.id.ivBack)
+
+        ivBack.setOnClickListener {
+            back()
+        }
+
+    }
+
+    fun back() {
+        val intent = Intent(this, LoginActivity::class.java);
+        startActivity(intent);
     }
 }
