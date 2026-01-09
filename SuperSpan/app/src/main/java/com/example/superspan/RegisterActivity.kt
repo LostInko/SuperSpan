6a64e470a6f2dbe71f21a0e19ff4ed0f2c398461
diiego-s <serradiego2005@icloud.com>
2026-01-09 10:07:42 +0100

    Added register screen and navigation from login.

--- SuperSpan/app/src/main/java/com/example/superspan/RegisterActivity.kt
+++ SuperSpan/app/src/main/java/com/example/superspan/RegisterActivity.kt
@@ -1,34 +1,20 @@
 package com.example.superspan
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val tvRegister = findViewById<TextView>(R.id.tvRegister)
-
-        tvRegister.setOnClickListener {
-            register()
-        }
-
-    }
-
-    fun register(){
-        val intent = Intent(this, RegisterActivity::class.java);
-        startActivity(intent);
     }
 }
