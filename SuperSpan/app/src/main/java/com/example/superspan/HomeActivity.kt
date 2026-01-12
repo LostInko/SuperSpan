b883f6da17e126a23269990953b98ddd9f45717c
Federicaa27 <fede.maiello27@gmail.com>
2026-01-12 17:02:08 +0100

    Updated the Home activity layout with a bottom navigation bar, personalized user greeting, and a custom background. Added several vector drawables for navigation icons (home, products, cart, favorites, profile) and included the AndroidX Fragment dependency. Corrected input properties for the username field in the registration screen.

--- SuperSpan/app/src/main/java/com/example/superspan/HomeActivity.kt
+++ SuperSpan/app/src/main/java/com/example/superspan/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.example.superspan
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,9 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val tvUserName = findViewById<TextView>(R.id.tvUserName)
+        tvUserName.text = com.example.superspan.GlobalData.user_list[0].name
+
     }
 }
