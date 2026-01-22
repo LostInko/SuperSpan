254a700386a010f42287c77fd418c60124c3609b
mikiichillo <chillottim@gmail.com>
2026-01-22 16:34:37 +0100

    aggiunte varie funzioni ai tasti nella pagina del prodotto (ancora da sistemare per bene)

--- SuperSpan/app/src/main/java/com/example/superspan/ui/fragment/ProductFragment.kt
+++ SuperSpan/app/src/main/java/com/example/superspan/ui/fragment/ProductFragment.kt
@@ -1,13 +1,22 @@
 package com.example.superspan.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.example.superspan.R
+import com.example.superspan.ui.activity.HomeActivity
+import com.example.superspan.ui.activity.MainActivity
 
 class ProductFragment : Fragment() {
+
+    var score = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +28,49 @@ class ProductFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product, container, false)
+        val view = inflater.inflate(R.layout.fragment_product, container, false)
+
+
+
+        val btnPlus = view.findViewById<ImageView>(R.id.btn_plus)
+        val btnMinus = view.findViewById<ImageView>(R.id.btn_minus)
+        val btnBack = view.findViewById<LinearLayout>(R.id.btn_back)
+        val numProd = view.findViewById<TextView>(R.id.productCount)
+
+        btnPlus.setOnClickListener {
+            addOne(numProd)
+        }
+        btnMinus.setOnClickListener {
+            minusOne(numProd)
+        }
+
+        btnBack.setOnClickListener {
+            home()
+        }
+
+        return view
     }
 
+    private fun addOne(numProdotti: TextView){
+        score++
+        numProdotti.text = "$score"
+    }
+    private fun minusOne(numProdotti: TextView){
+        if(score != 0) score--
+        numProdotti.text = "$score"
+    }
+
+    private fun home() {
+        // Creiamo un'istanza del fragment
+        val fragment = HomeFragment()
+
+        // Lo inseriamo nel contenitore dell'Activity
+        parentFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragment) // 'fragment_container' è l'ID nel tuo XML
+            addToBackStack(null) // Opzionale: permette di tornare indietro con il tasto back
+            commit()
+        }
+
+
+    }
 }
