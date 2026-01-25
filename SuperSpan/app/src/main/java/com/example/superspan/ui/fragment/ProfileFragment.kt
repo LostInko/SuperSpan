fc176a3bb6f467b9ae7201ef68fd5e997a11262e
giuliaacannata05 <g.cannata4@studenti.unica.it>
2026-01-25 06:22:54 +0100

    profilo + popup logout

--- SuperSpan/app/src/main/java/com/example/superspan/ui/fragment/ProfileFragment.kt
+++ SuperSpan/app/src/main/java/com/example/superspan/ui/fragment/ProfileFragment.kt
@@ -6,17 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import com.example.superspan.R
 import com.example.superspan.ui.activity.MainActivity
 import com.google.android.material.card.MaterialCardView
-import kotlin.jvm.java
 
 class ProfileFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,25 +20,39 @@ class ProfileFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
-        // Bind
+        // Binding dei componenti
         val cardLogout = view.findViewById<MaterialCardView>(R.id.card_logout)
         val cardDate = view.findViewById<MaterialCardView>(R.id.card_data)
         val cardOrder = view.findViewById<MaterialCardView>(R.id.card_order)
 
-
-        //Clicco su Logout
-        // AGGIUNGERE POP-UP DI SICUREZZA
+        // Clicco su Logout
         cardLogout.setOnClickListener {
-            // 2. Torna al Login
-            val intent = Intent(activity, MainActivity::class.java)
-            // Queste flag puliscono lo "stack" così l'utente non può tornare indietro col tasto back
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
+            val customView = layoutInflater.inflate(R.layout.dialog_layout, null)
+            val dialog = AlertDialog.Builder(requireContext())
+                .setView(customView)
+                .create()
 
-            // Chiude l'activity corrente (quella che ospita il fragment del profilo)
-            activity?.finish()
+            customView.findViewById<Button>(R.id.btn_annulla).setOnClickListener {
+                dialog.dismiss()
+            }
+
+            customView.findViewById<Button>(R.id.btn_conferma).setOnClickListener {
+                // Logica logout qui
+                // 2. Torna al Login
+                val intent = Intent(requireActivity(), MainActivity::class.java)
+
+                // Pulisce lo stack: l'utente non può tornare indietro
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+
+                // Chiude l'activity corrente
+                requireActivity().finish()
+            }
+
+            dialog.show()
         }
 
         return view
     }
 }
+
